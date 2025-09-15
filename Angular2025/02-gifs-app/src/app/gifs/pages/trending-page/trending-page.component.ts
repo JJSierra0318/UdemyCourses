79463cf58f937125db4664a641cd98d881e0624c
Juan José Sierra <juanjosesierram@gmail.com>
2025-09-15 18:19:39 -0500

    Preservar estado del scroll

--- Angular2025/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ Angular2025/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,16 +1,24 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
-import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
   // imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService)
 
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
+
+  // Executes just after the component is initialized
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement
+    if (!scrollDiv) return
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState()
+  }
 
   onScroll = (event: Event) => {
     const scrollDiv = this.scrollDivRef()?.nativeElement
@@ -21,9 +29,10 @@ export default class TrendingPageComponent {
     const scrollHeight = scrollDiv.scrollHeight
 
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight
+    this.scrollStateService.trendingScrollState.set(scrollTop)
 
     if (isAtBottom) {
       this.gifService.loadTrendingGifs()
     }
-  }
+  } 
 }
