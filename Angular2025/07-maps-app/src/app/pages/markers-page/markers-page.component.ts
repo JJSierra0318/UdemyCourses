dcac8d05f6baa68a3992ecd7fcce5c5496e2f6fc
Juan José Sierra <juanjosesierram@gmail.com>
2025-09-25 16:03:06 -0500

    Handle and show markers on map

--- Angular2025/07-maps-app/src/app/pages/markers-page/markers-page.component.ts
+++ Angular2025/07-maps-app/src/app/pages/markers-page/markers-page.component.ts
@@ -1,15 +1,23 @@
+import { JsonPipe } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
-import maplibregl from 'maplibre-gl'
+import maplibregl, { LngLatLike } from 'maplibre-gl'
+import { v4 as uuid } from 'uuid'
+
+interface Marker {
+  id: string;
+  mapMarker: maplibregl.Marker
+}
 
 @Component({
   selector: 'app-markers-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './markers-page.component.html',
 })
 export class MarkersPageComponent implements AfterViewInit {
 
   divElement = viewChild<ElementRef>('map');
   map = signal<maplibregl.Map | null>(null);
+  markers = signal<Marker[]>([])
 
   ngAfterViewInit() {
     new Promise((resolve) => setTimeout(() => resolve, 80));
@@ -25,7 +33,7 @@ export class MarkersPageComponent implements AfterViewInit {
       zoom: 14,
     })
 
-    const marker = new maplibregl.Marker({
+    /* const marker = new maplibregl.Marker({
       draggable: false,
       color: 'purple'
     })
@@ -34,12 +42,47 @@ export class MarkersPageComponent implements AfterViewInit {
 
     marker.on('dragend', (event) => {
 
-    })
+    }) */
 
     this.mapListeners(map);
   }
 
   mapListeners(map: maplibregl.Map) {
 
+    map.on('click', (event) => { this.mapClick(event) });
+
+    this.map.set(map);
+  }
+
+  mapClick(event: maplibregl.MapMouseEvent) {
+
+    if (!this.map()) return;
+    const map = this.map()!;
+    const coords = event.lngLat;
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const mapMarker = new maplibregl.Marker({
+      draggable: false,
+      color
+    })
+    .setLngLat(coords)
+    .addTo(map);
+
+    const newMarker: Marker = {
+      id: uuid(),
+      mapMarker
+    };
+
+    this.markers.update((markers) => [...markers, newMarker]);
+  }
+
+  flyToMarker(lngLat: LngLatLike) {
+    if (!this.map()) return;
+
+    this.map()?.flyTo({
+      center: lngLat
+    });
   }
 }
