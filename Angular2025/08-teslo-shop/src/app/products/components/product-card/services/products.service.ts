2b90d43871fc74d1db00f4987b262a5bcd5027e2
Juan José Sierra <juanjosesierram@gmail.com>
2025-10-09 16:18:15 -0500

    Cache for individual products

--- Angular2025/08-teslo-shop/src/app/products/components/product-card/services/products.service.ts
+++ Angular2025/08-teslo-shop/src/app/products/components/product-card/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '../interfaces/product.interface';
-import { Observable, tap } from 'rxjs';
+import { delay, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -17,9 +17,16 @@ export class ProductsService {
 
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductsResponse>();
+  private productCache = new Map<string, Product>();
+  
   getProducts(options: Options): Observable<ProductsResponse> {
 
     const { limit = 10, offset = 0, gender = '' } = options;
+    const key = `${limit}-${offset}-${gender}`
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!)
+    }
 
     return this.http
       .get<ProductsResponse>(`${baseUrl}/products`, {
@@ -30,11 +37,20 @@ export class ProductsService {
         }
       })
       .pipe(
-        tap((res) => console.log(res))
+        tap((res) => console.log(res)),
+        tap((res) => this.productsCache.set(key, res)),
       );
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
+    if (this.productCache.has(idSlug)) {
+      return of(this.productCache.get(idSlug)!);
+    }
+
     return this.http.get<Product>(`${baseUrl}/products/${idSlug}`)
+    .pipe(
+      delay(500),
+      tap((product) => this.productCache.set(idSlug, product)),
+    );
   }
 }
