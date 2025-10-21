47d153507f18c1f9d7b5f0e6f77b67cd14038ffe
Juan José Sierra <juanjosesierram@gmail.com>
2025-10-21 09:54:30 -0500

    Admin dashboard page and guard

--- Angular2025/08-teslo-shop/src/app/app.routes.ts
+++ Angular2025/08-teslo-shop/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { IsAdminGuard } from '@auth/guards/is-admin.guard';
 import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
@@ -8,6 +9,10 @@ export const routes: Routes = [
     canMatch: [
       NotAuthenticatedGuard,
     ]
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'),
   },
   {
     path: '',
