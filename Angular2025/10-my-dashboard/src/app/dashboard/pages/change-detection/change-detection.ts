ac85486487c0965a5cd01b4f875290f56d13894b
Juan José Sierra <juanjosesierram@gmail.com>
2025-11-06 16:35:06 -0500

    Initial change detection page

--- Angular2025/10-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
+++ Angular2025/10-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,14 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { Title } from '@shared/title/title';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
-  templateUrl: './change-detection.html',
-  styles: `
-    :host {
-      display: block;
-    }
+  imports: [Title, JsonPipe],
+  template: `
+  <app-title [title]="currentFramework()" />
+  <pre> {{ frameworkSignal() | json }} </pre>
+  <pre> {{ frameworkProperty | json }} </pre>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ChangeDetection { }
+export default class ChangeDetection {
+
+  currentFramework = computed(
+    () => `Change detection - ${this.frameworkSignal().name}`
+  )
+
+  frameworkSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  })
+
+  frameworkProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  }
+
+  constructor() {
+    setTimeout(() => {
+      //this.frameworkProperty.name = 'React';
+      this.frameworkSignal.update(value => ({
+        ...value,
+        name: 'React'
+      }))
+      console.log('Hecho');
+    }, 3000)
+  }
+}
